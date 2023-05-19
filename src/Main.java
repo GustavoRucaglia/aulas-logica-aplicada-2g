ccbc6c0d117c910b8b1516c8c8a9a25313bc825e
rm87162 <gustavorucaglia@gmail.com>
2023-05-19 09:06:02 -0300

    criando classe nome

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        //laço enquanto
-        int contador = 1;
-        while(contador < 10){
-            //processo
-            System.out.println("Contador =" + contador);
-            contador++;
-        }
-        //faça enquanto
-        int cont = 0;
-        do {
-           System.out.println(cont);
-           cont++;
-        }while(cont < 10);
+        //aula introdutoria de POO
 
-        //faça para
-        for( int x = 40; x <= 10; x++){
-            System.out.println(x);
-        }
-        }
+      }
     }
