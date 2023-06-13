42c40fc5e14fb901153373692e8fb86ef4edb3a4
rm87162 <gustavorucaglia@gmail.com>
2023-06-13 12:27:57 -0300

    alterações aula dia 13/06

---
+++ src/Planta.java
@@ -0,0 +1,5 @@
+public class Planta {
+    public Planta(semente s1) {
+
+    }
+}
---
+++ src/Solo.java
@@ -0,0 +1,12 @@
+public class Solo {
+
+    boolean fertil;
+    String tiposolo;
+    String LocalDoSolo;
+    String estado;
+
+    public Planta gerarPlanta(semente s1){
+        Planta p1 = new Planta(s1);
+        return p1;
+    }
+}
